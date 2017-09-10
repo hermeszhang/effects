7dcf74676827fdcef0044e8ae8cfbc29a23416af
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-10 13:15:53 -0400

    corrected printf.h header

--- src/printf.c
+++ src/printf.c
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "printf.h"
+#include <gnu-guitar/effects/printf.h>
 
 #include <stdio.h>
 #include <stdarg.h>
