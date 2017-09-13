174af8e59383fe6f975bc3ec2c1906a4b616aa2c
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-13 17:49:23 -0400

    added latency test

--- src/test.c
+++ src/test.c
@@ -4,6 +4,25 @@
 
 #include "test.h"
 
+#ifdef __unix__
+#include <time.h>
+#endif
+
+#ifdef __unix__
+struct timespec time_difference(const struct timespec *start, const struct timespec *end)
+{
+  struct timespec tmp;
+	if ((end->tv_nsec - start->tv_nsec) < 0) {
+		tmp.tv_sec = end->tv_sec - start->tv_sec - 1;
+		tmp.tv_nsec = 1000000000 + end->tv_nsec - start->tv_nsec;
+	} else {
+		tmp.tv_sec = end->tv_sec - start->tv_sec;
+		tmp.tv_nsec = end->tv_nsec - start->tv_nsec;
+	}
+	return tmp;
+}
+#endif
+
 int main(int argc, const char **argv) {
 
   int i;
@@ -13,6 +32,13 @@ int main(int argc, const char **argv) {
   struct gnu_guitar_packet packet;
   float in[256];
   float out[256];
+
+#ifdef __unix__
+  int err;
+  struct timespec start_time;
+  struct timespec end_time;
+  struct timespec diff_time;
+#endif
 
   memset(in, 0, sizeof(in));
 
@@ -50,9 +76,29 @@ int main(int argc, const char **argv) {
     return EXIT_FAILURE;
   }
 
+#ifdef __unix__
+  err = clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
+  if (err != 0) {
+    perror("Failed to get start time");
+    return EXIT_FAILURE;
+  }
+#endif
+
   for (j = 0; j < (rate / packet.len); j++) {
     gnu_guitar_effect_filter(effect, &packet);
   }
+
+#ifdef __unix__
+  err = clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
+  if (err != 0) {
+    perror("Failed to get end time");
+    return EXIT_FAILURE;
+  }
+  diff_time = time_difference(&start_time, &end_time);
+  printf("Total latency: %lu.%09lu seconds.\n",
+         diff_time.tv_sec,
+         diff_time.tv_nsec);
+#endif
 
   gnu_guitar_effect_free(effect);
 
